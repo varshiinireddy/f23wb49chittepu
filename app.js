eb6d55586aa784946b493a46a22ed6d476549a54
Chittepu Varshini <varshinireddy963@gmail.com>
2023-11-01 20:24:02 -0500

    Commited all files

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var instrumentsRouter = require('./routes/instruments');
+var instrumentsRouter = require('./routes/instrument');
+var boardRouter = require('./routes/board');
 
 var app = express();
 
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/instruments', instrumentsRouter);
+app.use('/instrument', instrumentsRouter);
+app.use('/board', boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/instrument.js
+++ routes/instrument.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('instruments', { title: 'Search Results' });
+  res.render('instrument', { title: 'Search Results' });
 });
 
 module.exports = router;
